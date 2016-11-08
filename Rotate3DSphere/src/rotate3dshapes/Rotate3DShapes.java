9b3c6873634c9c8dbc373cbabe0264584290fe31
Sándor György <sandor.gyorgy@gmail.com>
2016-11-08 14:53:45 +0100

    Mouse event handling is added.

--- Rotate3DSphere/src/rotate3dshapes/Rotate3DShapes.java
+++ Rotate3DSphere/src/rotate3dshapes/Rotate3DShapes.java
@@ -1,3 +1,7 @@
+// https://en.wikipedia.org/wiki/Equirectangular_projection#Reverse
+
+
+
 package rotate3dshapes;
 
 import java.awt.*;
@@ -25,7 +29,7 @@ import com.jogamp.opengl.util.texture.TextureIO;
  */
 @SuppressWarnings("serial")
 public class Rotate3DShapes extends GLCanvas
-        implements GLEventListener, KeyListener
+        implements GLEventListener, KeyListener,  MouseListener, MouseMotionListener
 {
     // Define constants for the top-level container
 
@@ -36,9 +40,11 @@ public class Rotate3DShapes extends GLCanvas
     
     private java.util.Random random;
     
+    private float mouseTestMove = 0.0f;
+    
     private String[] textureImages = new String[]
     {
-        home + "\\Documents\\NetBeansProjects\\javahome\\WorldMap.png",
+        home + "\\Documents\\NetBeansProjects\\javahome\\WorldMap.jpg",
     };
 
     private int[] textures = new int[textureImages.length];
@@ -112,6 +118,8 @@ public class Rotate3DShapes extends GLCanvas
         random = new java.util.Random();
         this.addGLEventListener(this);
         this.addKeyListener(this); // for Handling KeyEvents
+        this.addMouseListener(this); // for Handling KeyEvents
+        this.addMouseMotionListener(this); // for Handling KeyEvents
         this.setFocusable(true);
         this.requestFocus();
     }
@@ -186,12 +194,17 @@ public class Rotate3DShapes extends GLCanvas
 
         // ----- Render the Color Cube -----
         gl.glLoadIdentity();                // reset the current model-view matrix
-        gl.glTranslatef(0.0f, 0.0f, -3.0f); // translate right and into the screen
+        gl.glTranslatef(0.0f, 0.0f, -5.0f - mouseTestMove); // translate right and into the screen
         // gl.glRotatef(angleCube, 1.0f, 1.0f, 1.0f); // rotate about the x, y and z-axes
 
-        gl.glRotatef(xrot, 1.0f, 1.0f, 1.0f);
-        gl.glRotatef(yrot, 0.0f, 1.0f, 0.0f);
-        gl.glRotatef(zrot, 0.0f, 0.0f, 1.0f);
+        gl.glRotatef(30f, 1.0f, 0.0f, 0.0f);
+        gl.glRotatef(-23.27f, 0.0f, 0.0f, 1.0f);
+        gl.glRotatef(xrot, 0.0f, 1.0f, 0.0f);
+        
+        
+//        gl.glRotatef(xrot, 1.0f, 1.0f, 1.0f);
+//        gl.glRotatef(yrot, 0.0f, 1.0f, 0.0f);
+//        gl.glRotatef(zrot, 0.0f, 0.0f, 1.0f);
 
         int SIZE = 120;
         
@@ -217,6 +230,11 @@ public class Rotate3DShapes extends GLCanvas
                 gl.glBegin(GL_QUADS); // of the color cube
                 // gl.glColor3d(random.nextInt(256)/256f, random.nextInt(256)/256f, random.nextInt(256)/256f); // green
                 // gl.glColor3d(0.5, (1.0d * l)/latitudeCount, (1.0d * m)/meridianCount); // green
+                
+//                gl.glEnable (GL_BLEND); 
+//                gl.glBlendFunc (GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+//                gl.glColor4d(1.0d, 1.0d, 1.0d, 0.9d);
+
                 gl.glTexCoord2d(m*mDiff*tX, l*lDiff*tY);
                 gl.glVertex3d(Math.sin(beta1) * Math.cos(alpha1), Math.sin(alpha1), Math.cos(beta1) * Math.cos(alpha1));
 
@@ -255,9 +273,9 @@ public class Rotate3DShapes extends GLCanvas
         currentOpening += speedOpening;
 
         //change the speeds here
-        xrot += 0.12f;
+        xrot += 0.42f;
         yrot += 0.31f;
-        zrot += 0.03f;
+        zrot += 0.1f;
 
     }
 
@@ -305,4 +323,42 @@ public class Rotate3DShapes extends GLCanvas
     public void keyReleased(KeyEvent e)
     {
     }
+    
+// Mouse events
+
+    // Do-nothing methods, but required nonetheless
+    @Override
+    public void mouseEntered(MouseEvent e) {
+    }
+
+    @Override    
+    public void mouseExited(MouseEvent e) {
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        if(e.getButton() == MouseEvent.BUTTON1) {
+            mouseTestMove += 0.3f;
+        } else if(e.getButton() == MouseEvent.BUTTON3) {
+            mouseTestMove -= 0.3f;
+        }
+        
+    }
+    
+    @Override
+    public void mouseDragged(MouseEvent e) {
+    }
+
+    @Override
+    public void mouseMoved(MouseEvent e) {
+    }
+    
 }
